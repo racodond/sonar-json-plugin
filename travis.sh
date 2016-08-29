82cd44c7594c52f957df07fcf7d2b9d7ff4ec21b
David RACODON <david.racodon@gmail.com>
2016-08-29 11:52:47 +0200

    Add integration tests

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 mvn -B clean install
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
   mvn -B sonar:sonar \
       -Dsonar.analysis.mode=issues \
       -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
@@ -10,6 +10,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
       -Dsonar.github.oauth=$GITHUB_TOKEN \
       -Dsonar.login=$SONAR_TOKEN
 
-elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
   mvn -B sonar:sonar -Dsonar.login=$SONAR_TOKEN
 fi
+
+cd its
+mvn -B clean install -Dsonar.runtimeVersion=$SQ_VERSION
